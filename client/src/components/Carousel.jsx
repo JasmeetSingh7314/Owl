cdc412082ea5745bda39288d632490cb574268a1
Jasmeet Singh <96142761+JasmeetSingh7314@users.noreply.github.com>
2023-08-03 01:28:09 +0530

    Add files via upload

---
+++ client/src/components/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Carousel = () => {
+  return (
+    <div className='relative'>
+      <h2>Carousel</h2>
+    </div>
+  )
+}
+
+export default Carousel
---
+++ client/src/page/Collection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Categories from '../components/Categories'
+import Games from '../components/Games'
+
+const Collection = () => {
+  return (
+    <div>
+        <Categories />
+        <Games />
+      
+    </div>
+  )
+}
+
+export default Collection
