1a05faa204c9fa0be2a8f2c87cc6a01f2ba03646
Jasmeet Singh <96142761+JasmeetSingh7314@users.noreply.github.com>
2023-08-24 00:10:17 +0530

    Delete client directory

--- client/readme.md
+++
@@ -1,4 +0,0 @@
-# The Owl
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
--- client/src/assets/boilerpictures/index.js
+++
@@ -1,5 +0,0 @@
-import daku from './daku.png';
-import sukhu from './surkhab.png';
-import sus from './sus.jpg';
-
-export {daku,sukhu,sus};
