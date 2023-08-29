c11800451592f1006a7d90cd6740d5a835c80bd6
Jasmeet Singh <96142761+JasmeetSingh7314@users.noreply.github.com>
2023-08-30 00:56:10 +0530

    Add files via upload

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Carousel from '../components/Carousel'
+
+import Form from '../components/Form'
+import { mintNFT } from '../utils/operation.tsx'
+
+// import TestingWallet from '../components/TestingWallet'
+const Home = () => {
+  // const RenderTabs=()=>{
+  //   const NFTS=collectNFT()
+    
+
+  // }
+  // useEffect(()=>{
+  //   const amount=2;
+  //   const metadata={
+  //     name: name,
+  //     description: description,
+  //     decimals: 0,
+  //     // symbol: symbol,
+
+  //   }
+  //   mintNFT(amount,metadata)
+
+  // },[])
+  return (
+    <div>
+        <Header />
+        <Banner />
+        {/* <Carousel /> */}
+        
+        <Form />
+        
+        
+      
+		
+      
+    </div>
+  )
+}
+
+export default Home
